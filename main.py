5e228bd441ead4e224565b392b13204aed9539a9
Davi Araujo <davi.araujo13356@gmail.com>
2023-03-23 22:49:28 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import http.server
+import socketserver
+
+PORT = 8080
+
+class HelloHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, World!')
+            return
+        else:
+            
+            self.send_error(404)
+
+with socketserver.TCPServer(("", PORT), HelloHandler) as httpd:
+    print("Server listening on port", PORT)
+    httpd.serve_forever()
